88bc4988161d594c9564af38d8ebbd29dbd8ab05
Lewis Gunsch <lewis@gunsch.ca>
2010-12-03 18:09:24 -0700

    Made index.php a little easier to read.
    Signed-off-by: Lewis Gunsch <lewis@gunsch.ca>

--- www/index.php
+++ www/index.php
@@ -1,8 +1,13 @@
 <?php
 
+/* Should normally resolve to the web document root,
+ * ./../../lib/VictoryCMSTestRunner.php or you can just set it to
+ * "path/to/web_root/test_lib/VictoryCMSRunner.php"
+ */
+$webRoot = ''.dirname(__DIR__).DIRECTORY_SEPARATOR;
+
 // Path: "../include/VictoryCMS.php"
-require_once dirname(__DIR__).DIRECTORY_SEPARATOR.
-	'lib'.DIRECTORY_SEPARATOR.'VictoryCMS.php';
+require_once $webRoot.'lib'.DIRECTORY_SEPARATOR.'VictoryCMS.php';
 
 // Change the below path to config.json as needed
 VictoryCMS\VictoryCMS::bootstrap('../config.json');
