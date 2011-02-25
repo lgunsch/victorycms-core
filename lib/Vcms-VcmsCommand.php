3852618e52273c3a777ab1f36003c9692b86811f
Mitchell Bosecke <mitchell.bosecke@greybox.co>
2011-02-24 18:33:09 -0700

    Added structure to VcmsCommand

---
+++ lib/Vcms-VcmsCommand.php
@@ -0,0 +1,65 @@
+<?php
+//
+//  VictoryCMS - Content managment system and framework.
+//
+//  Copyright (C) 2010,2011  Lewis Gunsch <lgunsch@victorycms.org>
+//  Copyright (C) 2010,2011  Mitchell Bosecke <mitchellbosecke@gmail.com>
+//
+//  This file is part of VictoryCMS.
+//
+//  VictoryCMS is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  VictoryCMS is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with VictoryCMS.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * VictoryCMS - VcmsCommand
+ *
+ * @filesource
+ * @category VictoryCMS
+ * @package  Core
+ * @author   Mitchell Bosecke <mitchellbosecke@gmail.com>
+ * @license  GPL http://www.gnu.org/licenses/gpl.html
+ * @link     http://www.victorycms.org/
+ */
+
+namespace Vcms;
+
+/**
+ * This is an abstract class for a VcmsCommand class
+ *
+ * @todo: fill in documentation
+ */
+abstract class VcmsCommand
+{
+	/**
+	 * 
+	 */
+	abstract protected function staging();
+	
+	/**
+	 * 
+	 */
+	abstract protected function execute();
+	
+	/**
+	 * 
+	 */
+	abstract protected function tearDown();
+	
+	/**
+	 *  
+	 */
+	abstract protected function result();
+	
+
+}
+?>
