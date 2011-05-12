e18900b9b70e02ae87589f68d31c6b6e917a6b6e
Lewis Gunsch <lewis@gunsch.ca>
2011-05-12 11:21:05 -0600

    Changed HTTP message constants to be more clearly named.
    Signed-off-by: Lewis Gunsch <lewis@gunsch.ca>

--- lib/view/Vcms-VcmsResponse.php
+++ lib/view/Vcms-VcmsResponse.php
@@ -177,47 +177,47 @@ class VcmsResponse
 
 	/**#@+
 	 *
-     * HTTP status codes.
+     * HTTP status code messages.
      * @var string
      */
-	const HTTP_100='Continue';
-	const HTTP_101='Switching Protocols';
-	const HTTP_200='OK';
-	const HTTP_201='Created';
-	const HTTP_202='Accepted';
-	const HTTP_203='Non-Authorative Information';
-	const HTTP_204='No Content';
-	const HTTP_205='Reset Content';
-	const HTTP_206='Partial Content';
-	const HTTP_300='Multiple Choices';
-	const HTTP_301='Moved Permanently';
-	const HTTP_302='Found';
-	const HTTP_303='See Other';
-	const HTTP_304='Not Modified';
-	const HTTP_305='Use Proxy';
-	const HTTP_306='Temporary Redirect';
-	const HTTP_400='Bad Request';
-	const HTTP_401='Unauthorized';
-	const HTTP_402='Payment Required';
-	const HTTP_403='Forbidden';
-	const HTTP_404='Not Found';
-	const HTTP_405='Method Not Allowed';
-	const HTTP_406='Not Acceptable';
-	const HTTP_407='Proxy Authentication Required';
-	const HTTP_408='Request Timeout';
-	const HTTP_409='Conflict';
-	const HTTP_410='Gone';
-	const HTTP_411='Length Required';
-	const HTTP_412='Precondition Failed';
-	const HTTP_413='Request Entity Too Large';
-	const HTTP_414='Request-URI Too Long';
-	const HTTP_415='Unsupported Media Type';
-	const HTTP_416='Requested Range Not Satisfiable';
-	const HTTP_417='Expectation Failed';
-	const HTTP_500='Internal Server Error';
-	const HTTP_501='Not Implemented';
-	const HTTP_502='Bad Gateway';
-	const HTTP_503='Service Unavailable';
-	const HTTP_504='Gateway Timeout';
-	const HTTP_505='HTTP Version Not Supported';
+	const HTTP_MSG_100 = 'Continue';
+	const HTTP_MSG_101 = 'Switching Protocols';
+	const HTTP_MSG_200 = 'OK';
+	const HTTP_MSG_201 = 'Created';
+	const HTTP_MSG_202 = 'Accepted';
+	const HTTP_MSG_203 = 'Non-Authorative Information';
+	const HTTP_MSG_204 = 'No Content';
+	const HTTP_MSG_205 = 'Reset Content';
+	const HTTP_MSG_206 = 'Partial Content';
+	const HTTP_MSG_300 = 'Multiple Choices';
+	const HTTP_MSG_301 = 'Moved Permanently';
+	const HTTP_MSG_302 = 'Found';
+	const HTTP_MSG_303 = 'See Other';
+	const HTTP_MSG_304 = 'Not Modified';
+	const HTTP_MSG_305 = 'Use Proxy';
+	const HTTP_MSG_306 = 'Temporary Redirect';
+	const HTTP_MSG_400 = 'Bad Request';
+	const HTTP_MSG_401 = 'Unauthorized';
+	const HTTP_MSG_402 = 'Payment Required';
+	const HTTP_MSG_403 = 'Forbidden';
+	const HTTP_MSG_404 = 'Not Found';
+	const HTTP_MSG_405 = 'Method Not Allowed';
+	const HTTP_MSG_406 = 'Not Acceptable';
+	const HTTP_MSG_407 = 'Proxy Authentication Required';
+	const HTTP_MSG_408 = 'Request Timeout';
+	const HTTP_MSG_409 = 'Conflict';
+	const HTTP_MSG_410 = 'Gone';
+	const HTTP_MSG_411 = 'Length Required';
+	const HTTP_MSG_412 = 'Precondition Failed';
+	const HTTP_MSG_413 = 'Request Entity Too Large';
+	const HTTP_MSG_414 = 'Request-URI Too Long';
+	const HTTP_MSG_415 = 'Unsupported Media Type';
+	const HTTP_MSG_416 = 'Requested Range Not Satisfiable';
+	const HTTP_MSG_417 = 'Expectation Failed';
+	const HTTP_MSG_500 = 'Internal Server Error';
+	const HTTP_MSG_501 = 'Not Implemented';
+	const HTTP_MSG_502 = 'Bad Gateway';
+	const HTTP_MSG_503 = 'Service Unavailable';
+	const HTTP_MSG_504 = 'Gateway Timeout';
+	const HTTP_MSG_505 = 'HTTP Version Not Supported';
 }
